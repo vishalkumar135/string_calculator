7ec406fee99e85c02692d44469ccf94e57ce508b
vishalkumar <kumarasvishal@gmail.com>
2025-04-14 08:58:17 +0530

    'returns sum for two comma-separated numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,6 +1,9 @@
 class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
-    return numbers.to_i
+    return numbers.to_i if !numbers.include?(",")
+
+    numbers.split(",").map(&:to_i).sum
   end
+
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -9,4 +9,8 @@ RSpec.describe StringCalculator do
   it 'return number for single input' do
     expect(StringCalculator.new.add("1").to_eq(1))
   end
+
+  it 'returns sum for two comma-separated numbers' do
+    expect(StringCalculator.new.add("1,2").to_eq(3))
+  end
 end
