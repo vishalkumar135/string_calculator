8695b0ad94b9bc91306b07a618d262f792bac961
vishalkumar <kumarasvishal@gmail.com>
2025-04-14 09:09:32 +0530

    raises exception for a single negative number

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,7 +2,20 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
 
-    numbers.split(",").map(&:to_i).sum
+    delimiter = /,|\n/
+    if numbers.start_with?("//")
+      header, numbers = numbers.split("\n", 2)
+      custom = header[2..]
+      delimiter = Regexp.escape(custom)
+    end
+
+    parts = numbers.split(/#{delimiter}/).map(&:to_i)
+    negatives = parts.select { |n| n < 0 }
+    unless negatives.empty?
+      raise "negative numbers not allowed #{negatives.join(',')}"
+    end
+
+    parts.sum
   end
 
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -17,4 +17,17 @@ RSpec.describe StringCalculator do
   it 'returns sum for any amount of comma-separated numbers' do
     expect(StringCalculator.new.add("1,2,3,4")).to eq(10)
   end
+
+  it 'supports custom delimiters in the format //;\n1;2' do
+    expect(StringCalculator.new.add("//;\n1;2")).to eq(3)
+  end
+
+  it 'raises exception for a single negative number' do
+    expect {StringCalculator.new.add("-1")}.to raise_error("negative numbers not allowed -1")
+  end
+
+  it "raises exception for multiple negative numbers" do
+    expect { StringCalculator.new.add("-1,-2") }
+      .to raise_error("negative numbers not allowed -1,-2")
+  end
 end
